8205004ca5a9a551beace86e635ff155bbbedf88
Shawn Wilsher <sdwilsh@fb.com>
2017-04-12 07:49:38 -0700

    Handle patches with a diff line in them
    Summary:
    ShipIt would incorrectly parse a patch file of a diff as more changes to apply,
    which usually ended poorly.
    
    I can't really explain how this hasn't blown up before, but we can stop
    potential future pain.
    
    Reviewed By: mcallahan
    
    Differential Revision: D4872182
    
    fbshipit-source-id: c46c399511ee7e966d3b84686b78f898c5860de3

--- src/shipit/ShipItUtil.php
+++ src/shipit/ShipItUtil.php
@@ -38,7 +38,7 @@ abstract class ShipItUtil {
     foreach (explode("\n", $patch) as $line) {
       $line = preg_replace('/(\r\n|\n)/', "\n", $line);
 
-      if (preg_match('@^diff --git [ab]/(.*?) [ab]/\1$@', trim($line))) {
+      if (preg_match('@^diff --git [ab]/(.*?) [ab]/\1$@', rtrim($line))) {
         if ($contents !== '') {
           yield $contents;
         }
---
+++ tests/shipit/ShipItUtilTest.php
@@ -0,0 +1,19 @@
+<?hh // strict
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+namespace Facebook\ShipIt;
+
+final class ShipItUtilTest extends BaseTest {
+  public function testDiffofDiffs(): void {
+    $patch = file_get_contents(__DIR__.'/git-diffs/diff-in-diff.patch');
+    $sections = Vector {};
+    $sections->addAll(ShipItUtil::parsePatch($patch));
+    $this->assertEquals(1, $sections->count(), 'Should only get one section!');
+  }
+}
