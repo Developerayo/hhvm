6be0fcac49f3a1f92bcd1b055cd87565c83a764a
Yunus Rahbar <yrahbar@fb.com>
2017-10-26 13:44:29 -0700

    Enable optional_shape_field in ShipIt
    Summary:
    These experimental flags will be on by default in hhvm 3.23.
    Closes https://github.com/facebook/fbshipit/pull/44
    
    Reviewed By: sdwilsh
    
    Differential Revision: D6164080
    
    Pulled By: yns88
    
    fbshipit-source-id: 7c8e663543d1b0e18bc3910e6acc4293bece6d23

--- src/shipit/phase/ShipItCLIArgument.php
+++ src/shipit/phase/ShipItCLIArgument.php
@@ -10,16 +10,16 @@
 namespace Facebook\ShipIt;
 
 type ShipItCLIArgument = shape(
-  'short_name' => ?string,
+  ?'short_name' => string,
   'long_name' => string,
   // If null, the function is considered deprecated
-  'description' => ?string,
+  ?'description' => string,
   // Set non-null if deprecated with a replacement
-  'replacement' => ?string,
+  ?'replacement' => string,
   // Handler function for when the option is set
-  'write' => ?(function(string):mixed),
+  ?'write' => (function(string):mixed),
   // Detect if a required option has been set; if this isn't provided, it will
   // be required on the command line. Specifying this function allows it to be
   // prefilled on the config object instead
-  'isset' => ?(function():bool),
+  ?'isset' => (function():bool),
 );
