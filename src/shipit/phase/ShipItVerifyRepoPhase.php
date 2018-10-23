42e3193978e0a0a967b1600088c634bdf9c7394c
Yunus Rahbar <yrahbar@fb.com>
2018-10-23 11:53:34 -0700

    Add copyright headers to files that were missing them
    Reviewed By: ainsleyh
    
    Differential Revision: D10512250
    
    fbshipit-source-id: c9751faf5640b2c32c2a08502721cd2d5ba51305

--- src/shipit/phase/ShipItVerifyRepoPhase.php
+++ src/shipit/phase/ShipItVerifyRepoPhase.php
@@ -1,5 +1,12 @@
 <?hh // strict
-
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
 namespace Facebook\ShipIt;
 
 final class ShipItVerifyRepoPhase extends ShipItPhase {
